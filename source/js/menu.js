b36d2850333203d0f4cfdb9708b372ac0911c3d4
macspark <57904588+Macspark@users.noreply.github.com>
2020-05-08 00:33:37 +0400

    Index Menu JS

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,6 +1,10 @@
 var header = document.querySelector('.page-header');
+var main = document.querySelector('.page-main');
 var navBtn = document.querySelector('.page-header__toggle');
 var headerToggled = 'page-header--toggled';
+var headerScrolled ='page-header--scrolled';
+var mainScrolled = 'page-main--scrolled';
+var offset = main.offsetTop;
 
 header.classList.remove('page-header--nojs');
 
@@ -9,5 +13,15 @@ navBtn.addEventListener('click', function() {
     header.classList.remove(headerToggled);
   } else {
     header.classList.add(headerToggled);
+  }
+});
+
+window.addEventListener("scroll", function() {
+  if (window.pageYOffset >= offset) {
+    header.classList.add(headerScrolled)
+    main.classList.add(mainScrolled)
+  } else {
+    header.classList.remove(headerScrolled);
+    main.classList.remove(mainScrolled)
   }
 });
